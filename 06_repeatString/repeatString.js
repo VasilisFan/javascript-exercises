fb005b5c8a2a6131843e2c2acc7e98a57fb2e3d4
VasilisFan <vasilisfan@gmail.com>
2025-09-27 15:02:51 +0300

    this one was a little bit tricky l would say.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(x,y) {
+    if(x<0 || y<0){
+        return "ERROR";
+    }else{
+        let text = ""
+        for (let i=0; i<y; i++){
+            text += x;
+        }
+        return text;
+        }
 
 };
 
