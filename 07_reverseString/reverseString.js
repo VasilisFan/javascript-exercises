ccdba9ab08f65fb0ffaba53b2d9ed896dab7a435
VasilisFan <vasilisfan@gmail.com>
2025-09-27 15:57:51 +0300

    Another one

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(x) {
+    let text = x;
+    let temp = "";
+    for(let i of text){
+        temp = i + temp;
+    }
+    return temp;
+    //return text.split("").reverse().join("");
 };
 
 // Do not edit below this line
