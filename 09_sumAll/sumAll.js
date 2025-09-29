b84980cc166e83adaa61d0a01bb7121bee4400f3
VasilisFan <vasilisfan@gmail.com>
2025-09-30 02:15:37 +0300

    That one was easy actually :D

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x , y) {
+    if (x<0 || y<0 || !Number.isInteger(x) || !Number.isInteger(y)){
+        return 'ERROR';
+    }else{
+    let max = x;
+    let min = y;
+    let sum = 0;
+    if (max<y){
+        max = y;
+        min = x;
+    }
+   for (let i=min; i<=max; i++){
+            sum += i;
+        }
+    return sum;
+    }
 };
 
 // Do not edit below this line
