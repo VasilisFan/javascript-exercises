9791f768e5da470cec51dcfe27d623ab0e01ac68
VasilisFan <vasilisfan@gmail.com>
2025-09-30 03:42:57 +0300

    Googling is a master skill :)

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let temp = (x - 32) * 5/9;
+  if (!Number.isInteger(temp)){
+    temp = Number.parseFloat(temp).toFixed(1);
+  }
+  return Number(temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let temp = (x * 9/5) + 32;
+  if (!Number.isInteger(temp)){
+    temp = Number.parseFloat(temp).toFixed(1);
+  }
+  return Number(temp);
 };
 
 // Do not edit below this line
