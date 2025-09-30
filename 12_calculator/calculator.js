7463b3e2cb77e9703758940d4720bab30b562db0
VasilisFan <vasilisfan@gmail.com>
2025-09-30 12:49:25 +0300

    calculator done!

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(x,y) {
+  return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	let sum = 0;
+  for (let i of x){
+    sum += i;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(x){
+  let mul = 1;
+  for (let j of x){
+    mul = mul * j;
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  let pow = 1;
+	for (let i=0; i<y; i++){
+       pow *= x; 
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x===0){
+    return 1;
+  }else{
+    let fac = 1;
+    for (let i=1; i<=x; i++){
+      fac *= i;
+    }
+    return fac;  
+  }
 };
 
 // Do not edit below this line
